5c4dc2e762b14f64f53f85b6c27ee2ce00fab074
Fawzi Abdulfattah <iifawzie@gmail.com>
2023-12-14 00:49:38 +0200

    chore: icnrease the job interval
    Signed-off-by: Fawzi Abdulfattah <iifawzie@gmail.com>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command(CheckMissedNotifications::class)->everySecond();
+         $schedule->command(CheckMissedNotifications::class)->everyThirtyMinutes();
     }
 
     /**
